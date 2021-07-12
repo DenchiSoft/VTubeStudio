28148208deb7001727f4fdddaac7ef0abb331239
DenchiSoft <30596090+DenchiSoft@users.noreply.github.com>
2021-07-12 21:10:52 +0200

    Create HotkeyAction.cs

---
+++ Files/HotkeyAction.cs
@@ -0,0 +1,18 @@
+/// <summary>
+/// Enum for the actions that can be triggered by hotkeys.
+/// </summary>
+public enum HotkeyAction
+{
+    Unset = -1,                  // Unset.
+    TriggerAnimation = 0,        // Play an animation.
+    ChangeIdleAnimation = 1,     // Change the idle animation.
+    ToggleExpression = 2,        // Toggle an expression.
+    RemoveAllExpressions = 3,    // Remove all expressions.
+    MoveModel = 4,               // Moves the model to the target position.
+    ChangeBackground = 5,        // Change the current background.
+    ReloadMicrophone = 6,        // Reload the current microphone.
+    ReloadTextures = 7,          // Reload the model texture.
+    CalibrateCam = 8,            // Calibrate Camera.
+    ChangeVTSModel = 9,          // Change VTS Model.
+    TakeScreenshot = 10
+}
