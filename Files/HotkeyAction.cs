f18c58bbf60348cb674085046233af46632d3ff2
Denchi <30596090+DenchiSoft@users.noreply.github.com>
2022-04-15 17:58:01 +0200

    Update HotkeyAction.cs

--- Files/HotkeyAction.cs
+++ Files/HotkeyAction.cs
@@ -3,19 +3,20 @@
 /// </summary>
 public enum HotkeyAction
 {
-    Unset = -1,                  // Unset.
-    TriggerAnimation = 0,        // Play an animation.
-    ChangeIdleAnimation = 1,     // Change the idle animation.
-    ToggleExpression = 2,        // Toggle an expression.
-    RemoveAllExpressions = 3,    // Remove all expressions.
-    MoveModel = 4,               // Moves the model to the target position.
-    ChangeBackground = 5,        // Change the current background.
-    ReloadMicrophone = 6,        // Reload the current microphone.
-    ReloadTextures = 7,          // Reload the model texture.
-    CalibrateCam = 8,            // Calibrate Camera.
-    ChangeVTSModel = 9,          // Change VTS Model.
-    TakeScreenshot = 10,         // Takes a screenshot with the screenshot settings previously set in the UI.
-    ScreenColorOverlay = 11,     // Activates/Deactivates model screen color overlay.
-    RemoveAllItems = 12,         // Removes items from the scene.
-    ToggleItemScene = 13         // Toggles an item scene.
+    Unset = -1,                      // Unset.
+    TriggerAnimation = 0,            // Play an animation.
+    ChangeIdleAnimation = 1,         // Change the idle animation.
+    ToggleExpression = 2,            // Toggle an expression.
+    RemoveAllExpressions = 3,        // Remove all expressions.
+    MoveModel = 4,                   // Moves the model to the target position.
+    ChangeBackground = 5,            // Change the current background.
+    ReloadMicrophone = 6,            // Reload the current microphone.
+    ReloadTextures = 7,              // Reload the model texture.
+    CalibrateCam = 8,                // Calibrate Camera.
+    ChangeVTSModel = 9,              // Change VTS Model.
+    TakeScreenshot = 10,             // Take a screenshot with the previous settings.
+    ScreenColorOverlay = 11,         // Activates/Deactivates model screen color overlay.
+    RemoveAllItems = 12,             // Removes all items from the scene.
+    ToggleItemScene = 13,            // Loads an item scene.
+    DownloadRandomWorkshopItem = 14  // Downloads a random item from the Steam Workshop and attempts to load it into the scene.
 }
