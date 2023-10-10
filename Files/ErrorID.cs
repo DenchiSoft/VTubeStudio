ae82973cfc1c42164edb5652161ca1acbece90ef
Denchi <30596090+DenchiSoft@users.noreply.github.com>
2023-10-10 16:18:14 +0200

    Update ErrorID.cs

--- Files/ErrorID.cs
+++ Files/ErrorID.cs
@@ -111,7 +111,8 @@ public enum ErrorID
     ItemOrderInvalid = 755,
     ItemOrderAlreadyTaken = 756,
     ItemLoadValuesInvalid = 757, // Invalid values for fields like size, position, ...
-
+    ItemCustomDataInvalid = 758, // Invalid values for custom data field (customDataBase64). Happens when data is provided here but it's no valid JPG/PNG data or image dimensions are invalid.
+    
     // Errors related to ItemUnloadRequest
     CannotCurrentlyUnloadItem = 800,  // This is usually because the user has menus open that prevent items from being unloaded.
 
